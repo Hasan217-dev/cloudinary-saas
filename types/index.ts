d613f20ffd03f6ef38e717920e6b0531765fcbd7
hassannawaz1420@gmail.com <hassannawaz1420@gmail.com>
2026-07-11 15:14:30 +0500

    feat: implement Home component with video fetching and layout, add AppLayout for navigation, and create VideoCard for video display

---
+++ types/index.ts
@@ -0,0 +1,11 @@
+export interface Videos {
+    id: string
+    title: string
+    description: string
+    public_Id: string
+    originalSize: number
+    compressedSize: number
+    duration: number
+    createdAt: Date
+    updatedAt: Date
+}
